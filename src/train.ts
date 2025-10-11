6995b77c0a030cb319d5a5e979b85208adcf0f12
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-12 01:09:00 +0900

    feat: TASK-H completed

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,20 @@
 console.log('======>TRAIN MODE<======');
+
+/**
+ TASK H: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+ */
+
+// masalani yechimi
+
+function getPositive(arr: number[]): string {
+  const positives: number[] = arr.filter((num) => num > 0);
+  return positives.join('');
+}
+
+const example_data = [1, -4, 2];
+const result = getPositive(example_data);
+console.log('result: ', result);
