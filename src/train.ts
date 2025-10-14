8e87eaf996ee18b97f7e2df2bd739cc07682bd8a
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-14 23:59:00 +0900

    feat: TASK H2 completed

--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,32 @@
 console.log('======>TRAIN MODE<======');
 
 /**
+ * TASK H2: 
+
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+
+MASALAN: getDigits("m14i1t") return qiladi "141"
+ */
+
+// masalani yechimi
+
+function getDigits(str: string): string {
+  let result = '';
+  for (let char of str) {
+    if (!isNaN(Number(char)) && char !== ' ') {
+      result += char;
+    }
+  }
+  return result;
+}
+
+// Example:
+const example_data = 'm14i1t';
+const result = getDigits(example_data);
+console.log('result: ', result);
+
+// =========================================================================
+/**
  TASK H: 
 
 shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
@@ -9,7 +35,7 @@ MASALAN: getPositive([1, -4, 2]) return qiladi "12"
  */
 
 // masalani yechimi
-
+/*
 function getPositive(arr: number[]): string {
   const positives: number[] = arr.filter((num) => num > 0);
   return positives.join('');
@@ -18,3 +44,4 @@ function getPositive(arr: number[]): string {
 const example_data = [1, -4, 2];
 const result = getPositive(example_data);
 console.log('result: ', result);
+*/
