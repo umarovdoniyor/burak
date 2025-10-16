9d142eefa01507e455e65838cfa0ff4cda991445
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-17 04:02:09 +0900

    feat develop signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { T } from '../libs/types/common';
 import MemberService from '../models/Member.service';
+import { MemberInput } from '../libs/types/member';
+import { MemberType } from '../libs/enums/member.enum';
 
 const restaurantController: T = {};
 
@@ -40,12 +42,21 @@ restaurantController.processLogin = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processSignup = (req: Request, res: Response) => {
+restaurantController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log('processSignup 🔥');
-    res.send('DONE : processSignup');
+    console.log('body: ', req.body);
+
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.RESTAURANT;
+
+    const memberService = new MemberService();
+    const result = await memberService.processSignup(newMember);
+
+    res.send(result);
   } catch (err) {
     console.log('Error processSignup:', err);
+    res.send(err);
   }
 };
 
