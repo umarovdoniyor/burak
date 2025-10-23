4eae496ebaa2b86cfad45208cfd9d190136aea89
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-23 22:00:36 +0900

    fix: create sessions on login and signup

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { T } from '../libs/types/common';
 import MemberService from '../models/Member.service';
-import { LoginInput, MemberInput } from '../libs/types/member';
+import { AdminRequest, LoginInput, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
 
 const memberService = new MemberService();
@@ -35,7 +35,10 @@ restaurantController.getLogin = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log('processSignup 🔥');
     console.log('body: ', req.body);
@@ -44,22 +47,29 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
     // TODO: SESSION AUTHENTICATIONS
-
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log('Error processSignup:', err);
     res.send(err);
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log('processLogin 🔥');
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
     // TODO: SESSION AUTHENTICATIONS
-
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log('Error processLogin:', err);
     res.send(err);
