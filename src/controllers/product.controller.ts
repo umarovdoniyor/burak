0dfb44847a0c658f7eee4c158bcd9ea5616094f1
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-25 21:19:47 +0900

    feat: locals variable integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { T } from '../libs/types/common';
 import Errors from '../libs/Errors';
 import ProductService from '../models/Product.service';
+import { AdminRequest } from '../libs/types/member';
 
 const productService = new ProductService();
 const productController: T = {};
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -16,9 +16,21 @@ routerAdmin.get('/logout', restaurantController.logout);
 routerAdmin.get('/check-me', restaurantController.checkAuthSession);
 
 /** Product */
-routerAdmin.get('/product/all', productController.getAllProducts);
-routerAdmin.get('/product/create', productController.createNewProduct);
-routerAdmin.get('/product/:id', productController.updateChosenProduct);
+routerAdmin.get(
+  '/product/all',
+  restaurantController.verifyRestaurant,
+  productController.getAllProducts
+);
+routerAdmin.get(
+  '/product/create',
+  restaurantController.verifyRestaurant,
+  productController.createNewProduct
+);
+routerAdmin.get(
+  '/product/:id',
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 
 /** User */
 
