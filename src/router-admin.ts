8b9f8ccaea7f647a858e9305834e17773a43b2ba
umarovdoniyor <mr.danny951008@gmail.com>
2025-10-25 22:19:42 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import restaurantController from './controllers/restaurant.controller';
 import productController from './controllers/product.controller';
+import makeUploader from './libs/utils/uploader';
+// import { uploadProductImage } from './libs/utils/uploader';
 const routerAdmin = express.Router();
 
 /** Restaurant */
@@ -10,7 +12,11 @@ routerAdmin
   .post('/login', restaurantController.processLogin);
 routerAdmin
   .get('/signup', restaurantController.getSignup)
-  .post('/signup', restaurantController.processSignup);
+  .post(
+    '/signup',
+    makeUploader('members').single('memberImage'),
+    restaurantController.processSignup
+  );
 
 routerAdmin.get('/logout', restaurantController.logout);
 routerAdmin.get('/check-me', restaurantController.checkAuthSession);
@@ -21,12 +27,14 @@ routerAdmin.get(
   restaurantController.verifyRestaurant,
   productController.getAllProducts
 );
-routerAdmin.get(
+routerAdmin.post(
   '/product/create',
   restaurantController.verifyRestaurant,
+  // uploadProductImage.single('productImage'),
+  makeUploader('products').array('productImages', 5),
   productController.createNewProduct
 );
-routerAdmin.get(
+routerAdmin.post(
   '/product/:id',
   restaurantController.verifyRestaurant,
   productController.updateChosenProduct
