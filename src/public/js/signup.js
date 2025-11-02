4040debfbdefbcaee3a2b9cf68951b6895ed4e18
umarovdoniyor <mr.danny951008@gmail.com>
2025-11-03 04:33:17 +0900

    feat: form validation and image upload & validation completed

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log('Signup frontend javascript file');
 
-$(function () {});
+$(function () {
+  const fileTarget = $('.file-box .upload-hidden');
+  let filename;
+
+  fileTarget.on('change', function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile['type'];
+      const validImageType = ['image/jpg', 'image/jpeg', 'image/png'];
+      if (!validImageType.includes(fileType)) {
+        alert('Please insert onlu jpeg, jpg and png!');
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $('.upload-img-frame')
+            .attr('src', URL.createObjectURL(uploadFile))
+            .addClass('success');
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings('.upload-name').val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $('.member-nick').val();
@@ -20,6 +43,14 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert('Password differs, please check!');
+    return false;
+  }
+
+  const memberImage = $('.member-image').get(0).files[0].name
+    ? $('.member-image').get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert('Please insert restaurant image!');
     return false;
   }
 }
