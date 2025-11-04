1258f35a02322b8c9bd89720f1b314b415d5420d
umarovdoniyor <mr.danny951008@gmail.com>
2025-11-05 03:58:42 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -12,14 +12,40 @@ $(function () {
     }
   });
 
+  // show new product addition btn logic
   $('#process-btn').on('click', () => {
     $('.dish-container').slideToggle(500);
     $('#process-btn').css('display', 'none');
   });
 
+  // hide new product addition btn logic
   $('#cancel-btn').on('click', () => {
     $('.dish-container').slideToggle(200);
     $('#process-btn').css('display', 'flex');
+  });
+
+  // product status update logic
+
+  $('.new-product-status').on('change', async function (e) {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    console.log('id:', id);
+    console.log('productStatus:', productStatus);
+
+    try {
+      const response = await axios.post(`/admin/product/${id}`, {
+        productStatus: productStatus,
+      });
+      console.log('response: ', response);
+      const result = response.data;
+      if (result.data) {
+        console.log('Product updated!');
+        $('.new-product-status').blur();
+      } else alert('Product update failed!');
+    } catch (err) {
+      console.log('Error: ', err);
+      alert('Product update failed!');
+    }
   });
 });
 
