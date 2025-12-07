82174655a9a4214e0193042b00912e7db039128a
Danny <mr.danny951008@gmail.com>
2025-12-07 22:50:30 +0900

    feat: TASK-ZD completed

--- src/train.ts
+++ src/train.ts
@@ -3,6 +3,37 @@ console.log("======>TRAIN MODE<======");
 // =========================================================================
 // =========================================================================
 // =========================================================================
+
+/**
+ *TASK ZD
+
+Shunday function yozing. Bu function o'ziga, parametr sifatida
+birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
+qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
+shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
+yangilangan arrayni qaytarsin.
+
+MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
+
+Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
+Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
+yangilangan arrayni qaytarmoqda.
+ */
+// solution
+
+function changeNumberInArray(
+  index: number,
+  arr: number[],
+  newValue: number
+): number[] {
+  const new_arr = [...arr]; // avoid mutating original
+  new_arr[index] = newValue; // change the value
+  return new_arr;
+}
+
+// Example
+console.log(changeNumberInArray(1, [1, 3, 7, 2], 2));
+
 // =========================================================================
 
 /**
@@ -20,7 +51,7 @@ Yoki 10 gradus Selsiy, 50 Farenhaytga teng.
 °C va °F => Tempraturani o'lchashda ishlatiladigan o'lchov birligi.
  */
 // solution
-
+/*
 function celsiusToFahrenheit(c: number): number {
   return (c * 9) / 5 + 32;
 }
@@ -28,7 +59,7 @@ function celsiusToFahrenheit(c: number): number {
 // Example
 console.log(celsiusToFahrenheit(0));
 console.log(celsiusToFahrenheit(10));
-
+*/
 // =========================================================================
 /**
  *TASK-ZB:
