4e670661af7429fd4b184407057db200af07ad58
Danny <mr.danny951008@gmail.com>
2025-12-28 01:14:45 +0900

    feat: TASK-ZL completed

--- src/train.ts
+++ src/train.ts
@@ -3,6 +3,21 @@ console.log("======>TRAIN MODE<======");
 // =========================================================================
 // =========================================================================
 // =========================================================================
+/**
+ *TASK-ZL:
+
+Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
+MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
+ */
+// solution
+
+function stringToKebab(input_str: string): string {
+  return input_str.toLowerCase().trim().replace(/\s+/g, "-");
+}
+
+// Example
+console.log(stringToKebab("I love Kebab"));
+
 // =========================================================================
 /**
  * TASK-ZK:
@@ -11,7 +26,7 @@ Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan
 MASALAN: printNumbers()
  */
 // solution
-
+/*
 function printNumbers(): void {
   let count = 1;
   const intervalId = setInterval(() => {
@@ -25,7 +40,7 @@ function printNumbers(): void {
 
 // Example
 printNumbers();
-
+*/
 // =========================================================================
 /**
  * TASK ZJ:
