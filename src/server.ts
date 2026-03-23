415b029bc7df1a74d2643c20b14ca8cd7d18e6b4
Danny <mr.danny951008@gmail.com>
2026-03-24 02:02:09 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import app from './app';
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
-    console.log('MongoDB connectin succeed');
+    console.log("MongoDB connectin succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
       console.info(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http:/localhost:${PORT}/admin \n`);
     });
   })
-  .catch((err) => console.log('ERROR on connection MongoDB', err));
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
